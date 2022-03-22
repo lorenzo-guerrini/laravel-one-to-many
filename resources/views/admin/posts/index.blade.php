5e267bd8a0c0a0beed9f28e8fd5511e189254a5f
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-22 22:25:09 +0100

    Adesso vengono mostrate le immagini caricate

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -14,7 +14,8 @@
                     <th>ID</th>
                     <th class="col-3">Title - Slug</th>
                     <th class="col-2">Category</th>
-                    <th class="col-5">Content</th>
+                    <th class="col-1">Image</th>
+                    <th class="col-4">Content</th>
                     <th class="text-center col-2">Actions</th>
                 </tr>
             </thead>
@@ -22,17 +23,31 @@
             <tbody>
                 @foreach ($posts as $post)
                     <tr>
+                        {{-- ID --}}
                         <td>{{ $post->id }}</td>
 
+                        {{-- Tittle --}}
                         <td>
                             <div><strong>{{ $post->title }}</strong></div>
                             <div>({{ $post->slug }})</div>
                         </td>
 
+                        {{-- Category --}}
                         <td class="text-center">
-                            {{ $post->category ? $post->category->name : "/"}}
+                            {{ $post->category ? $post->category->name : '/' }}
                         </td>
 
+                        {{-- Image --}}
+                        <td class="text-center">
+                            @if (isset($post->image))
+                                <img style="width: 20px" src="{{ asset("storage/{$post->image}") }}"
+                                    alt="{{ $post->title }}">
+                            @else
+                                /
+                            @endif
+                        </td>
+
+                        {{-- Content --}}
                         <td>
                             {{ substr($post->content, 0, 150) }}
                             @if (strlen($post->content) > 150)
@@ -40,6 +55,7 @@
                             @endif
                         </td>
 
+                        {{-- Buttons --}}
                         <td class="text-center">
                             <a class="btn btn-primary btn-sm mb-1"
                                 href="{{ route('admin.posts.show', ['post' => $post->id]) }}">More Info</a>
--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -14,8 +14,21 @@
             <li><strong>ID: </strong>{{ $post->id }}</li>
             <li><strong>Title: </strong>{{ $post->title }}</li>
             <li><strong>Slug: </strong>{{ $post->slug }}</li>
+
+            @if ($post->category)
+                <li><strong>Category: </strong>{{ $post->category->name }}</li>
+            @endif
+
             <li><strong>Created at: </strong>{{ $post->created_at }}</li>
             <li><strong>Updated at: </strong>{{ $post->updated_at }}</li>
+
+            @if ($post->image)
+                <li>
+                    <strong>Image:</strong><br>
+                    <img class="img-fluid" src="{{ asset("storage/{$post->image}") }}" alt="{{ $post->title }}">
+                </li>
+            @endif
+
             <li><strong>Content: </strong><br>{{ $post->content }}</li>
         </ul>
     </div>
