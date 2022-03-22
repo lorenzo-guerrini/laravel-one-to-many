3e2adce1b89f703bc54aca2e2a7f42205e7a555a
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-03-22 21:49:06 +0100

    Aggiunto caricamento immagime, nuovi errori e +

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -20,7 +20,7 @@
             @endif
         </div> --}}
 
-        <form action="{{ route('admin.posts.store') }}" method="POST">
+        <form action="{{ route('admin.posts.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
 
             <div class="form-group">
@@ -28,19 +28,30 @@
                 <input type="text" id="title" name="title" class="form-control @error('title') is-invalid @enderror"
                     value="{{ old('title') }}" placeholder="New post's title">
                 @error('title')
-                    <div class="alert alert-danger">{{ $message }}</div>
+                    <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
 
             <div class="form-group">
                 <label for="category">Category</label>
-                <select name="category_id" id="category_id" class="form-control">
-                    <option value="">--- Select Category ---</option>
+                <select name="category_id" id="category_id" class="form-control @error('category_id') is-invalid @enderror">
+                    <option value="">No Category</option>
                     @foreach ($categories as $category)
                         <option value="{{ $category->id }}" {{ $category->id == old('category_id') ? 'selected' : '' }}>
                             {{ $category->name }}</option>
                     @endforeach
                 </select>
+                @error('category_id')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="image">Image</label>
+                <input type="file" name="image" id="image" class="form-control @error('image') is-invalid @enderror">
+                @error('image')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="form-group">
@@ -48,7 +59,7 @@
                 <textarea id="content" name="content" class="form-control @error('content') is-invalid @enderror" rows="10"
                     placeholder="New post's content">{{ old('content') }}</textarea>
                 @error('content')
-                    <div class="alert alert-danger">{{ $message }}</div>
+                    <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
 
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -22,7 +22,8 @@
             @endif
         </div> --}}
 
-        <form action="{{ route('admin.posts.update', ['post' => $post->id]) }}" method="POST">
+        <form action="{{ route('admin.posts.update', ['post' => $post->id]) }}" method="POST"
+            enctype="multipart/form-data">
             @csrf
             @method('PUT')
 
@@ -31,20 +32,31 @@
                 <input type="text" id="title" name="title" class="form-control @error('title') is-invalid @enderror"
                     placeholder="Post's title" value="{{ old('title', $post->title) }}">
                 @error('title')
-                    <div class="alert alert-danger">{{ $message }}</div>
+                    <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
 
             <div class="form-group">
                 <label for="category">Category</label>
-                <select name="category_id" id="category_id" class="form-control">
-                    <option value="">--- Select Category ---</option>
+                <select name="category_id" id="category_id" class="form-control @error('category_id') is-invalid @enderror">
+                    <option value="">No Category</option>
                     @foreach ($categories as $category)
                         <option value="{{ $category->id }}"
                             {{ $category->id == old('category_id', $post->category_id) ? 'selected' : '' }}>
                             {{ $category->name }}</option>
                     @endforeach
                 </select>
+                @error('category_id')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <label for="image">Image</label>
+                <input type="file" name="image" id="image" class="form-control @error('image') is-invalid @enderror">
+                @error('image')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
             </div>
 
             <div class="form-group">
@@ -52,7 +64,7 @@
                 <textarea id="content" name="content" class="form-control @error('content') is-invalid @enderror" rows="10"
                     placeholder="Post's content">{{ old('content', $post->content) }}</textarea>
                 @error('content')
-                    <div class="alert alert-danger">{{ $message }}</div>
+                    <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
 
